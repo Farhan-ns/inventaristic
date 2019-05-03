7f6814039b2433661ca643d27a4c331dca597981
farhan-ns <farhanunnasih@gmail.com>
2019-05-03 13:55:27 +0700

    forgotten

--- src/com/smkn4/inventaristic/admin/siswa/DataPeminjam.java
+++ src/com/smkn4/inventaristic/admin/siswa/DataPeminjam.java
@@ -34,7 +34,7 @@ public class DataPeminjam extends javax.swing.JFrame {
         readData();
     }
     
-    private void readData() {
+    protected void readData() {
         String[] kolomTabel = {"NIS", "Nama","Tanggal Lahir", "Jenis Kelamin", "Kelas", "Jurusan", "Tahun Ajaran"};
         defaultTableModel   = new DefaultTableModel(null, kolomTabel);
         try {
--- src/com/smkn4/inventaristic/admin/siswa/ManageEdit.java
+++ src/com/smkn4/inventaristic/admin/siswa/ManageEdit.java
@@ -324,9 +324,7 @@ public class ManageEdit extends javax.swing.JDialog {
         }catch (SQLException e){
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Terjadi kesalahan di Database");
-        } catch (ParseException ex) {
-        Logger.getLogger(ManageEdit.class.getName()).log(Level.SEVERE, null, ex);
-    }
+        }
     }
     /**
      * @param args the command line arguments
