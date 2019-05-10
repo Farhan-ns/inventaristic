846bf64e6e9136b123af00992f7f0a47974e4e70
ariyadiaip <ariyadiaip@gmail.com>
2019-05-10 10:44:21 +0700

    Perbaikan 1

--- src/com/smkn4/inventaristic/user/peminjaman/pilihanMenu.java
+++ src/com/smkn4/inventaristic/user/peminjaman/pilihanMenu.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor.SMKN(BDG
  */
 package com.smkn4.inventaristic.user.peminjaman;
 
