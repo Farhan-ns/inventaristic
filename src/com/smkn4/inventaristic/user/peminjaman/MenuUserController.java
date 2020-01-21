fe866537eb4a9c0e2991f85ec24b5dae6bc11abc
farhan-ns <farhanunnasih@gmail.com>
2020-01-21 07:34:16 +0700

    lbl nama update

--- src/com/smkn4/inventaristic/user/peminjaman/MenuUserController.java
+++ src/com/smkn4/inventaristic/user/peminjaman/MenuUserController.java
@@ -55,7 +55,7 @@ public class MenuUserController implements Initializable {
     }   
     
     private void setIdentitas() {
-        lblNama.setText("Hallo, " + map.get("nama").toString() + "!");
+        lblNama.setText("Hallo, " + map.get("nama_kelas").toString() + "!");
     }
     
     private void setButtonsAction() {
